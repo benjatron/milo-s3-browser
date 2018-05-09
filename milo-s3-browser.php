3160c2098c45b656b20a7d6fc5725779574a110d
Ben Davidson <ben@bendavidson.me>
2018-05-09 16:30:50 -0400

    Added plugin header and aws loader

---
+++ milo-s3-browser.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Milo S3 Browser
+Description:  S3 Bucket Browser for MILO Range
+Author:       FWD Creative, LLC
+Author URI:   https://designfwd.com/
+License:      GPL3
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  milo
+*/
+
+require 'aws/aws-autoloader.php';
