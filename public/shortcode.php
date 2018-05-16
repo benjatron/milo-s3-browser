8cf9f14aa1b54925eed73b5bd3d00befc7ce2eec
Ben Davidson <ben@bendavidson.me>
2018-05-16 17:12:06 -0400

    Reworked code to test credentials

--- public/shortcode.php
+++ public/shortcode.php
@@ -1,36 +1,24 @@
 <?php
+
+use Aws\S3\S3Client;
+
 // Shortcode to display S3 bucket
 function milo_s3_browser_shortcode_display($atts) {
-  $aws_key = esc_attr( get_option('aws_key') );
-  $aws_secret = esc_attr( get_option('aws_secret') );
-  $aws_region = esc_attr( get_option('aws_region') );
 
-  // Creates a credential provider
-  function env() {
-
-    // This IS the credential provider
-    return function() {
-      $key = $aws_key;
-      $secret = $aws_secret;
-      if($key && $secret) {
-        return Promise\promise_for(
-          new Credentials($key, $secret, getenv(self::ENV_SESSION))
-        );
-      }
-
-      $msg = 'Could not find environment variable '
-        . 'credentials in ' . self::ENV_KEY . '/' . self::ENG_SECRET;
-      return new RefectedPromise(new CredentialsException($msg));
-    };
+  //Creates S3 client
+  $credentials = new Aws\Credentials\Credentials(
+    esc_attr( get_option('aws_key') ),
+    esc_attr( get_option('aws_secret') )
+  );
+  $s3Client = new S3Client([
+    'region' => esc_attr( get_option('aws_region') ),
+    'version' => 'latest',
+    'credentials' => $credentials
+  ]);
+  $buckets = $s3Client->listBuckets();
+  foreach ($buckets['Buckets'] as $bucket){
+	  echo $bucket['Name']."\n";
   }
-
-  // Creates the AWS credentials
-  $result = $stsClient->getSessionToken();
-
-  // Displays error is any values are missing
-  if( $aws_key == '' || $aws_secret == '' || $aws_region == '' ):
-    echo "Make sure your access key, access secret, and region are all set correctly.";
-  endif;
 
   // If no bucket name is specified for the shortcode, set default
   $atts = shortcode_atts( array('bucket' => 'none'), $atts, 's3browse' );
@@ -43,13 +31,10 @@ function milo_s3_browser_shortcode_display($atts) {
   ?>
   <div class="o-fileBrowser">
     <div class="o-fileBrowser__manager">
-
       <ul class="m-fileList">
+        <li>Filename: </li>
+        <li>Download</li>
       </ul>
-
-      <div class="m-fileList m-fileList--empty">
-        No files present. If you're expecting to see something here, check to make sure your bucket settings are correct.
-      </div>
 
     </div>
   </div>
