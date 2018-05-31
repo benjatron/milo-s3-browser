3319573240540622d429678cd6d89f722532d87a
Ben Davidson <ben@bendavidson.me>
2018-05-31 17:23:30 -0400

    Added script registration/enqueueing

--- milo-s3-browser.php
+++ milo-s3-browser.php
@@ -12,6 +12,14 @@ Text Domain:  milo
 // Load AWS SDK files
 require 'vendor/autoload.php';
 
+function milo_register_scripts() {
+  wp_enqueue_script( 's3-browser', plugins_url( 'assets/scripts/dist/main.min.js', __FILE__), array('sage/js') );
+
+  wp_register_style('s3-browser', plugins_url( 'assets/styles/dist/main.css', __FILE__), array('sage/css') );
+  wp_enqueue_style('s3-browser');
+}
+add_action( 'wp_enqueue_scripts', 'milo_register_scripts');
+
 // Load admin dashboard files
 require (dirname(__FILE__) . '/admin/functions.php');
 require (dirname(__FILE__) . '/admin/settings-page.php');
