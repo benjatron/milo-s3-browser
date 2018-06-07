c77e18df26d5d0127d5615beb078d96b6f3b6d95
Ben Davidson <ben@bendavidson.me>
2018-06-07 12:47:10 -0400

    Added random_bytes polyfill

--- admin/functions.php
+++ admin/functions.php
@@ -10,5 +10,25 @@ function milo_s3_browser_settings() {
   register_setting( 'milo-s3-browser-plugin', 'aws_key');
   register_setting( 'milo-s3-browser-plugin', 'aws_secret');
   register_setting( 'milo-s3-browser-plugin', 'aws_region');
+  register_setting( 'milo-s3-browser-plugin', 'milo_persistent_key');
 }
 add_action('admin_init', 'milo_s3_browser_settings');
+
+// Creates a new cron schedule every month
+function milo_cron_schedule( $schedule ) {
+  $schedule['every-month'] = array(
+    'interval' => 1 * MONTH_IN_SECONDS,
+    'display' => __( 'Every month', 'milo')
+  );
+  return $schedule;
+}
+add_filter( 'cron_schedules', 'milo_cron_schedule' );
+
+// Schedules the auto-generated password cron event
+function milo_cron_passgen() {
+
+}
+add_action( 'milo_cron_hook' , 'milo_cron_passgen' );
+if( !wp_next_scheduled( 'milo_cron_hook') ):
+  wp_schedule_event( time() ,'every-month', 'milo_cron_hook');
+endif;
--- admin/settings-page.php
+++ admin/settings-page.php
@@ -17,10 +17,10 @@ function milo_s3_browse_display_settings() {
         MILO Range - S3 Browser Settings
       </h1>
       <p>
-        Please set your AWS access key and secret below, along with the region that your target bucket(s) reside in. Make sure you have the proper IAM permissions or your data will not display!
+        Please set your AWS access key and secret below, along with the region that your target bucket(s) reside in. Make sure you have the proper IAM permissions or your data will not display! The simplest way to show a bucket is with a shortcode, with the following format: <code>[milos3browser bucket=s3-bucket-name]</code>
       </p>
       <p>
-        The simplest way to show a bucket is with a shortcode, with the following format: <code>[milos3browser bucket=s3-bucket-name]</code>
+        You can also set an administrator password for accessing the S3 content below, as well as see an auto-generated key that is updated every 30 days.
       </p>
       <p>
         Huge thanks to Dan Dulaney aka <a href="https://github.com/duplaja" target="_blank">duplaja</a> for making his S3 bucket browser plugin open for public scrutiny. This plugin would not exist without it.
@@ -30,7 +30,7 @@ function milo_s3_browse_display_settings() {
         <tr>
           <td colspan="3">
             <h2>
-              Amazon S3 Settings (Required)
+              Amazon S3 Settings
             </h2>
           </td>
         </tr>
@@ -70,6 +70,42 @@ function milo_s3_browse_display_settings() {
           <td>
             <p>
               The region your bucket(s) reside in. Probably <code>us-east-1</code>
+            </p>
+          </td>
+        </tr>
+      </table>
+
+      <table class="form-table">
+        <tr>
+          <td colspan="3">
+            <h2>
+              Content Protection Settings
+            </h2>
+          </td>
+        </tr>
+        <tr valign="top">
+          <th scope="row">
+            Administrator Password
+          </th>
+          <td>
+            <input type="text" name="milo_persistent_key" value="<?php echo esc_attr( get_option('milo_persistent_key') ); ?>" />
+          </td>
+          <td>
+            <p>
+              Enter a password for administrators to use for accessing content here.
+            </p>
+          </td>
+        </tr>
+        <tr valign="top" class="separator">
+          <th scope="row">
+            Auto-Generated Password
+          </th>
+          <td>
+            <input type="text" name="milo_generated_key" value="<?php echo esc_attr( get_option('milo_generated_key') ); ?>" />
+          </td>
+          <td>
+            <p>
+              This password can also be used for accessing content and can be shared for short-term purposes. It is automatically regenerated every 30 days.
             </p>
           </td>
         </tr>
