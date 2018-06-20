ab55836104f13fb1928dc2777ef149a8b6f962e8
Ben Davidson <ben@bendavidson.me>
2018-06-20 18:17:39 -0400

    Added post password update

--- app/helpers/password-generator.php
+++ app/helpers/password-generator.php
@@ -4,4 +4,12 @@ function milo_password_generator() {
   $option = 'milo_generated_key';
   $passphrase = esc_attr( base64_encode( random_bytes( 12 ) ) );
   update_option( $option, $passphrase );
+
+  $browserPages = milo_browser_pages();
+  foreach( $browserPages as $page ):
+    wp_update_post( array(
+      'ID' => $page,
+      'post_password' => $passphrase
+    ) );
+  endforeach;
 }
