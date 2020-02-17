937af27bc85d2895355001d00be222357a664dae
Ben Davidson <ben@bendavidson.me>
2020-02-17 15:21:01 -0500

    Updated queries

--- app/helpers/browser-query.php
+++ app/helpers/browser-query.php
@@ -1,30 +1,24 @@
 <?php
 function milo_browser_pages() {
-  $dashboards = get_posts( array(
-    'post_type' => 'page',
-    'fields' => 'ids',
-    'meta_key' => '_wp_page_template',
-    'meta_value' => 'template-browser-dashboard.php'
-  ) );
   $browsers = get_posts( array(
     'post_type' => 'page',
     'fields' => 'ids',
     'meta_key' => '_wp_page_template',
-    'meta_value' => 'template-browser-page.php'
+    'meta_value' => 'page-templates/support-browser.php'
+  ) );
+  $dashboards = get_posts( array(
+    'post_type' => 'page',
+    'fields' => 'ids',
+    'meta_key' => '_wp_page_template',
+    'meta_value' => 'page-templates/support-dashboard.php'
+  ) );
+  $downloads = get_posts( array(
+    'post_type' => 'page',
+    'fields' => 'ids',
+    'meta_key' => '_wp_page_template',
+    'meta_value' => 'page-templates/support-download.php'
   ) );
 
-  // An array of the page slugs for landing pages
-  $slugs = array(
-    'milo-content',
-    'milo-software',
-    'milo-videos'
-  );
-  // Add these pages to an array
-  $pages = array();
-  foreach ( $slugs as $page ):
-    $pages[] = get_page_by_path($page)->ID;
-  endforeach;
-
-  $pageGroup = array_merge( $dashboards, $browsers, $pages );
+  $pageGroup = array_merge( $browsers, $dashboards, $downloads );
   return $pageGroup;
 }
