70923f62ac57f3ae1d4e5decf34f30f3826d8f4f
Ben Davidson <ben@bendavidson.me>
2020-05-31 16:41:51 -0400

    Updated class documentation, added get_objects()

--- app/classes/class-milo-support.php
+++ app/classes/class-milo-support.php
@@ -30,6 +30,9 @@ class MILO_Support {
   // S3 client
   public $s3_client;
 
+  // Page query
+  public $query = array();
+
   function __construct() {
     $this->aws_key = get_option( 'aws_key' );
     $this->aws_secret = get_option( 'aws_secret' );
@@ -48,22 +51,88 @@ class MILO_Support {
       'version' => 'latest',
       'credentials' => $this->aws_credentials
     ]);
+
+    // Sets the page query
+    $r = $_SERVER['REQUEST_URI']; 
+    parse_str( substr( $r, strpos( $r, '?' ) + 1 ), $this->query );
   }
 
-  // Returns the AWS buckets
+  /**
+   * @return array AWS buckets tied to the current S3 client
+   */
   public function get_buckets() {
-    return $this->s3_client->listBuckets([]);
+    $buckets = array();
+
+    // Get the Buckets object
+    $object = $this->s3_client->listBuckets([])['Buckets'];
+
+    // Add just the bucket names to our $buckets array
+    foreach( $object as $bucket ):
+      $buckets[] = $bucket['Name'];
+    endforeach;
+
+    // Return just the Buckets array
+    return $buckets;
   }
 
-  // Returns a specific file, given an input bucket name
+  /**
+   * Gets the download URL for a specific file, given a bucket
+   * 
+   * @var string $filename      The key of the file to query
+   * @var string $bucket        The AWS bucket to search in
+   * 
+   * @return string A URI to download the requested file
+   */
   public function get_file( $filename, $bucket ) {
-    return $this->s3_client->getObject([
-      'Bucket'      => $bucket,
-      'Key'         => $filename
-    ]);
+    // Get the filetype
+    $base = basename( $key );
+
+    // Create a request for the file's URI
+    $cmd = $this->s3_client->getCommand( 'GetObject',
+      [
+        'Bucket'                        => $bucket,
+        'Key'                           => $filename,
+        'ResponseContentType'           => 'application/octet-stream',
+        'ResponseContentDisposition'    => 'attachment; filename="' . $base . '"'
+      ]
+    );
+    $request = $this->s3_client->createPresignedRequest( $cmd, '+180 minutes' );
+    $uri = $request->getUri();
+
+    // Return a string of the full URI needed to download the file
+    return (string) $uri ;
   }
 
-  // Returns the password to access files
+  /**
+   * Gets all object keys in a bucket
+   * 
+   * @var string $bucket      The AWS bucket to query
+   * 
+   * @return array The keys of all objects in the bucket
+   */
+  public function get_objects( $bucket ) {
+    // Creates an array to add keys to
+    $keys = array();
+
+    // Gets all objects in a bucket
+    $results = $this->s3_client->getPaginator( 'ListObjects', [
+      'Bucket'    => $bucket
+    ]);
+
+    // Adds object keys to $keys array
+    foreach( $results as $result ):
+      foreach( $result['Contents'] as $object ):
+        $keys[] = $object['Key'];
+      endforeach;
+    endforeach;
+
+    // Return the $keys array
+    return $keys;
+  }
+
+  /** 
+   * @return string The password to access support portal pages
+   */
   public function get_password() {
     return $this->password;
   }
